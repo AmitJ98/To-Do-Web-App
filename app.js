5ee7b04f65103cea8149aa2673c46cb1db1825bc
AmitJ98 <143286440+AmitJ98@users.noreply.github.com>
2024-05-01 15:59:21 +0300

    build some basic html files

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ import express from 'express';
 
 
 const app = express()
+
+//enable css url link in the html
+app.use('/public', express.static('public'));
+
 app.listen(3000)
 
+
+
 app.get('/' , (requset,response) => {
-    console.log("the app is working")
-    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\home_page.html')
+    console.log("home page")
+    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\home.html')
+});
+
+
+app.get('/about' ,(requset,response) => {
+    console.log("about page")
+    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\About.html')
+});
+
+
+app.get('/SignUp' ,(requset,response) => {
+    console.log("signup page")
+    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\SignUp.html')
 });
