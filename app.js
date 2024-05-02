209246c5a05adc36299e8c3255fd1bcc0bc79089
AmitJ98 <143286440+AmitJ98@users.noreply.github.com>
2024-05-02 17:46:05 +0300

    updated pages to a dynamic page

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 import express from 'express';
+import { render } from 'ejs';
 
 
 const app = express()
-
-//enable css url link in the html
-app.use('/public', express.static('public'));
-
 app.listen(3000)
+
+
+app.set('view engine', 'ejs')
+
+
+app.use('/public', express.static('public')); //enable css url link in the html
+app.use(express.urlencoded({ extended: true }));
+
 
 
 
 app.get('/' , (requset,response) => {
-    console.log("home page")
-    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\home.html')
+    response.render('home');
 });
 
 
 app.get('/about' ,(requset,response) => {
-    console.log("about page")
-    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\About.html')
+    response.render('about');
 });
 
-
 app.get('/SignUp' ,(requset,response) => {
-    console.log("signup page")
-    response.sendFile('C:\\Users\\Amit Joseph\\Desktop\\Java_Script\\To-Do-Web-App\\views\\SignUp.html')
+    response.render('SignUp')
+});
+
+app.post('/SignUp' ,(requset,response) => {
+    console.log(requset.body);
+    response.redirect("/");
+} );
+
+
+app.get('/lists' ,(requset,response) => {
+    const tasks = [{task:'clean the room' ,description:'bla bla bla bla'},
+    {task:'do my homework' ,description:'who does homework'}
+    ]
+    response.render('Lists',{tasks: tasks});
 });
